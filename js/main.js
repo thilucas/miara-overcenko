b7c1fc58b8a6a17966f070e06ede87f0c601695b
thilucas <44647212+thilucas@users.noreply.github.com>
2019-11-05 19:11:17 -0300

    Innitial Commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: -25.095065, lng: -50.15853 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 16,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    this.document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
